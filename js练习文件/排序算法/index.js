7abd915294faa4417f4cfa5d04261db8b40956aa
liudong <58164700+ruyanwangshi@users.noreply.github.com>
2022-10-12 21:51:05 +0800

    feat:  增加插入排序算法

--- js练习文件/排序算法/index.js
+++ js练习文件/排序算法/index.js
@@ -59,31 +59,31 @@
 //   }
 // }
 
-function mySort(arr) {
-  if (Array.isArray(arr)) {
-    let len = arr.length
-    let index = 0
-    let changeFlag = false
-    while (len-- > 0) {
-        let minIndex = index
-        for (let i = index + 1; i < arr.length; i++) {
-           if(arr[minIndex] > arr[i]) {
-            minIndex = i
-            changeFlag = true
-           }
-        }
-        if(minIndex !== index) {
-          const pre = arr[minIndex]
-          arr[minIndex] = arr[index]
-          arr[index] = pre
-        }
-        index++
-    };
-    return arr
-  }
-}
+// function mySort(arr) {
+//   if (Array.isArray(arr)) {
+//     let len = arr.length
+//     let index = 0
+//     let changeFlag = false
+//     while (len-- > 0) {
+//         let minIndex = index
+//         for (let i = index + 1; i < arr.length; i++) {
+//            if(arr[minIndex] > arr[i]) {
+//             minIndex = i
+//             changeFlag = true
+//            }
+//         }
+//         if(minIndex !== index) {
+//           const pre = arr[minIndex]
+//           arr[minIndex] = arr[index]
+//           arr[index] = pre
+//         }
+//         index++
+//     };
+//     return arr
+//   }
+// }
 
-console.log(mySort([4, 3, 2, 1]))
+// console.log(mySort(arr))
 
 // console.log(mySort([4, 3, 2, 1]))
 
@@ -102,19 +102,52 @@ console.log(mySort([4, 3, 2, 1]))
 // }
 
 // async function total() {
-//   const res1 = await sum(1, 2, 3, 4)
+//   const res1 = await sum(1, 2, 3, 4, 5, 6)
 //   const res2 = await sum(1, 2, 3)
 //   return [res1, res2]
 // }
-
+// console.time()
 // total().then((res) => {
 //   console.log(res)
+//   console.timeEnd()
 // })
 
 // async function sum(...arg) {
-//   let sumValue
-//   let count
-//   let index = 0
+//   // let sumValue
+//   // let count
+//   // let index = 0
+
+//   const newObj = {}
+//   let i = 0
+//   let res
+//   if (Array.isArray(arg)) {
+//     while (i < arg.length) {
+//       const value = arg[i]
+//       newObj[value] = arg[++i] || 0
+//       i++
+//     }
+//     const task = []
+//     // console.log(newObj)
+//     for (const item in newObj) {
+//       // console.log(item, newObj[item])
+//       task.push(() => {
+//         return new Promise((resolve) => {
+//           asyncAdd(+item, newObj[item], (_, res) => {
+//             resolve(res)
+//           })
+//         })
+//       })
+//     }
+//     res = await Promise.all(task.map((fn) => fn())).then((res) => {
+//       if(res.length > 1) {
+//         return sum(...res)
+//       } else {
+//         return res[0]
+//       }
+//     })
+//   }
+
+//   // return res
 //   while (index < arg.length) {
 //     if (!sumValue) {
 //       sumValue = arg[index++]
@@ -129,3 +162,29 @@ console.log(mySort([4, 3, 2, 1]))
 //   }
 //   return sumValue
 // }
+
+// 插入排序
+
+function mySort(list) {
+  if (Array.isArray(list)) {
+    let item
+    for (let i = 1; i < list.length; i++) {
+      item = list[i]
+      let index = i
+      for (let j = i - 1; j >= 0; j--) {
+        if (item < list[j]) {
+          console.log(list[j])
+          list[index] = list[j]
+          index = j
+        }
+        
+      }
+
+      list[index] = item
+
+    }
+    return list
+  }
+}
+
+console.log(mySort([4,3,2,1]))
