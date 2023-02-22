e23ec295d355fba4240f9092991bd2b3c532776e
liudong <58164700+ruyanwangshi@users.noreply.github.com>
2023-02-22 13:33:09 +0800

    style: 推

--- js动画/拖拽动画/index.js
+++ js动画/拖拽动画/index.js
@@ -1,19 +1,55 @@
-const list = new Array(9).fill(0).map((_,index) => `${index}${index}${index}`);
+const list = new Array(9).fill(0).map((_, index) => `${index}${index}${index}`)
 console.log(list)
+const activeOptions = {
+  x: 0,
+  y: 0,
+}
 
-const root = document.getElementById("root");
-
+const root = document.getElementById('root')
 
 render(root)
 
-function createDom(type, content) {
-    const dom = document.createElement('type');
-    dom.innerHTML = content
-    return dom
+function createDom(vnode, root) {
+  const dom = document.createElement(vnode.type)
+
+  if (vnode.props) {
+    for (const key in vnode.props) {
+        console.log('key',key)
+      if (/^on/.test(key)) {
+        dom.addEventListener(key.match(/[^on](.)*/g)[0].toLowerCase(), vnode.props[key])
+      }
+    }
+  }
+  if (Array.isArray(vnode.children)) {
+    vnode.children.forEach((element) => {
+      createDom(element, dom)
+    })
+  } else {
+    dom.innerHTML = vnode.children
+  }
+  console.log(root)
+  root.appendChild(dom)
+  return dom
 }
 
-
 function render(root) {
-    const nodes = list.map(item => createDom('div', item))
-    root.append(nodes)
+  const nodes = list.map((item) => ({
+    type: 'div',
+    props: {
+      onPointerMove(e) {
+        console.log(e)
+      },
+      onClick(e) {
+        console.log(1111111)
+      }
+    },
+    children: item,
+  }))
+  createDom(
+    {
+      type: 'div',
+      children: nodes,
+    },
+    root
+  )
 }
